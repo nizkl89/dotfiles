8e5d51856c8adb632151dfa85dff1fca319c10b8
nizkl89 <lkinz7789@gmail.com>
2026-07-30 17:26:10 +0800

    Simplify aerospace-sketchybar integration: direct query, no env var tricks, pin workspaces 7-9 to main display

--- .config/sketchybar/plugins/aerospace.sh
+++ .config/sketchybar/plugins/aerospace.sh
@@ -1,12 +1,8 @@
 #!/usr/bin/env bash
 
-# make sure it's executable with:
-# chmod +x ~/.config/sketchybar/plugins/aerospace.sh
-if [ -z "$FOCUSED_WORKSPACE" ]; then
-  FOCUSED_WORKSPACE=$(aerospace query-workspaces --focused)
-fi
+FOCUSED=$(aerospace list-workspaces --focused)
 
-if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
+if [ "$1" = "$FOCUSED" ]; then
   sketchybar --set "$NAME" background.drawing=on
 else
   sketchybar --set "$NAME" background.drawing=off
